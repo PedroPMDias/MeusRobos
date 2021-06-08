dee4d14142f20e826eec3cf84eed87ee5aa565e5
PedroPMDias <diaspedro.dev@gmail.com>
2021-06-08 03:51:24 -0300

    arrumar o erro no backend de runtimeError no dict

--- ClimaTempo/API/Servidor.py
+++ ClimaTempo/API/Servidor.py
@@ -2,21 +2,23 @@ import json, os
 
 class Backend():
     def __init__(self, lugar):
-        self.chavedojson = str(lugar.replace(' ', '').replace('/', '_')).title()
+        self.chavedoJson = str(lugar.replace(' ', '').replace('/', '_')).upper()
         self.pasta = str(os.path.dirname(os.path.realpath(__file__)))
         self.dir_Previsoes = self.pasta.replace('API', 'Previsoes')
         self.dir_Registros = self.pasta.replace('API', 'Registros')
         self.historico = self.dir_Registros + '/historico.json'
 
-    def _titulacao(self, novoJson):
-        urlsNovas = novoJson.values()
-        for title, url in urlsNovas:
-            urlsNovas[str(f"{self.dir_Previsoes}/{title}")] = url
-            urlsNovas.pop(title)
-        return novoJson
+    def _titulacao(self, thisJson):
+        jsonValues = dict(thisJson)
+        
+        for title, url in jsonValues.items(): ### RuntimeError: dictionary keys changed during iteration
+            jsonValues[str(f"{self.dir_Previsoes}/{title}")] = url
+            jsonValues.pop(title)
+            
+        return thisJson
 
-    def renovarUrls(self, novoLocal):
-        novoJson = self._titulacao(novoLocal)
+    def renovarUrls(self, jsonLocal):
+        novoJson = self._titulacao(jsonLocal)
         with open(self.historico, 'w', encoding='utf-8') as arquivo:
             json.dump(novoJson, arquivo, indent=4, ensure_ascii=False)
         return novoJson
@@ -25,24 +27,28 @@ class Backend():
         with open(self.historico, 'r') as arquivo:
             leitura = json.load(arquivo)
             try:
-                urls = leitura[self.chavedojson]
-            except KeyError as ke:
-                print(f"Não achei o local -> {ke}. Mas vou baixar e renovar o conteudo todo")
+                urls = leitura[self.chavedoJson]
+            except Exception as erro:
+                print(f"Não achei o local -> {erro}. Mas vou baixar e renovar o conteudo todo")
                 return False
             else:
-                print('Server found local ->', self.chavedojson)
+                print('Server found local ->', self.chavedoJson)
                 return urls
 
     def prever_arquivos(self, arquivos):
-        dir_local = self.dir_Previsoes + '/' + self.chavedojson
         if not os.path.exists(self.dir_Previsoes):
-            os.mkdir(self.dir_Previsoes) and os.mkdir(dir_local)
+            dir_local = self.dir_Previsoes + '/' + self.chavedoJson
+            os.mkdir(self.dir_Previsoes)
+            os.mkdir(dir_local)
 
         chaves = arquivos.values()
         for chave in chaves.keys():
             if not os.path.exists(chave):
                 print(chave)
-"""
+
+'''
 back = Backend("Rio de Janeiro/RJ")
-print(back.pegarUrls())
-"""
+dict_urls = back.pegarUrls()
+urls_renovadas = back.renovarUrls(dict_urls)
+print(back.prever_arquivos(urls_renovadas))
+'''
