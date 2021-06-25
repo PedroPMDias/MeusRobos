c877335134b7170f38d4acf4c8af28434084d688
PedroPMDias <diaspedro.dev@gmail.com>
2021-06-24 21:44:20 -0300

    nova  estrutura

--- ClimaTempo/main.py
+++ ClimaTempo/main.py
@@ -1,22 +1,6 @@
 # Importação das bibliotecas
-from API.Servidor import Backend
-from API.Internet import Frontend
+from API.robot import Backend
 
-# Cidade da busca ##lugar = "Rio das Ostras/RJ"
-lugar = "Rio de Janeiro/RJ"
+back = Backend("Colombo/PR")
 
-# Instaciando as classes
-back, front = Backend(lugar), Frontend(lugar)
-
-# Buscando no servidor
-dict_urls = back.pegar_Urls()
-
-# Buscando na internet e salvando novo json
-if not isinstance(dict_urls, dict):
-    back.renovarUrls(front.pegar_Urls())
-    dict_urls = back.pegar_Urls()
-
-# Verificar a existencia dos arquivos
-print(front.download_html(back.existencia_arquivos(dict_urls)))
-
-'''###Montar o html geral com todos os fragmentos'''
+print(back.requisitor())
